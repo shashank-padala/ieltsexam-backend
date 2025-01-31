a5284712779720fe496b21aa29fcefeb2c7e8a90
Shashank Padala <shashank.padala24@gmail.com>
2025-01-31 14:41:15 -0500

    added core modules - users, exams, questions, attempts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ExamsModule } from './exams/exams.module';
+import { QuestionsModule } from './questions/questions.module';
+import { AttemptsModule } from './attempts/attempts.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ExamsModule, QuestionsModule, AttemptsModule],
   controllers: [AppController],
   providers: [AppService],
 })
